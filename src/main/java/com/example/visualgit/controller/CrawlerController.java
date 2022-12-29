d1bd416910ccec1fa92a5343dfb15f8fa1275eaf
lsmxtc <12012939@mail.sustech.edu.cn>
2022-12-29 20:43:31 +0800

    after

--- src/main/java/com/example/visualgit/controller/CrawlerController.java
+++ src/main/java/com/example/visualgit/controller/CrawlerController.java
@@ -2,13 +2,12 @@ package com.example.visualgit.controller;
 
 import com.example.visualgit.entity.Result;
 import com.example.visualgit.service.crawler.ContextFactory;
+import java.io.IOException;
+import java.util.Objects;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.io.IOException;
-import java.util.Objects;
 
 @Controller
 @CrossOrigin
@@ -16,20 +15,21 @@ import java.util.Objects;
 public class CrawlerController {
 
     public static boolean isFinished = false;
+
     @GetMapping("/do_crawl")
     public Result crawlData(String url) throws IOException {
         String s = url;
-        int k=0;
+        int k = 0;
         while (!isFinished) {
-            k=k+1;
-            if(url.contains("issues")) {
+            k = k + 1;
+            if (url.contains("issues")) {
                 url = s + "?per_page=100&state=all&page=" + k;
-            }else {
+            } else {
                 url = s + "?per_page=100&page=" + k;
             }
             Objects.requireNonNull(ContextFactory.getContext(url)).doCrawl(url);
         }
-        isFinished=false;
+        isFinished = false;
         return Result.ok().code(200);
     }
 
--- src/main/java/com/example/visualgit/controller/RepositoryController.java
+++ src/main/java/com/example/visualgit/controller/RepositoryController.java
@@ -1,15 +1,15 @@
 package com.example.visualgit.controller;
+
 import com.example.visualgit.entity.Result;
 import com.example.visualgit.service.RepositoryService;
+
+import java.text.ParseException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.text.ParseException;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @CrossOrigin
@@ -20,47 +20,47 @@ public class RepositoryController {
     RepositoryService service;
 
     @GetMapping("/developer_quantity")
-    public Result getDeveloperQuantityByRepositoryId(String id){
+    public Result getDeveloperQuantityByRepositoryId(String id) {
         return service.getDeveloperQuantity(id);
     }
 
     @GetMapping("developer_rank")
-    public Result getDeveloperRankByRepositoryId(String id){
+    public Result getDeveloperRankByRepositoryId(String id) {
         return service.getMostActiveDeveloper(id);
     }
 
     @GetMapping("openissue")
     public Result getOpenIssue(String repos) throws ParseException {
-        return service.showIssue("open",repos);
+        return service.showIssue("open", repos);
     }
 
     @GetMapping("closeissue")
     public Result getCloseIssue(String repos) throws ParseException {
-        return service.showIssue("closed",repos);
+        return service.showIssue("closed", repos);
     }
 
     @GetMapping("release")
-    public Result showReleaseCommission(String id){
+    public Result showReleaseCommission(String id) {
         return service.showReleaseCommission(id);
     }
 
     @GetMapping("analyseCommitByDay")
-    public Result analyseCommitByDay(String id){
+    public Result analyseCommitByDay(String id) {
         return service.analyseCommitByDay(id);
     }
 
     @GetMapping("analyseCommitByWeek")
-    public Result analyseCommitByWeek(String id){
+    public Result analyseCommitByWeek(String id) {
         return service.analyseCommitByWeek(id);
     }
 
     @GetMapping("getAllRepo")
-    public Result getAllRepo(){
+    public Result getAllRepo() {
         return service.getAllRepo();
     }
 
     @GetMapping("analyseCommitByHour")
-    public Result analyseCommitByHour(String id){
+    public Result analyseCommitByHour(String id) {
         return service.analyseCommitByHour(id);
     }
 
